6e7e1f1c6035ebe2e89d1618dcd48159d41751ce
Jillian Maher <jillian.maher2@boone.kyschools.us>
2019-12-12 12:29:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 var question = "Which of the following is a school color?";
+var answers = ['Red', 'Green', 'Blue', 'Yellow'];
+var answerButtons = document.getElementsByClassName('quizButton');
+var correct = 3;
+
+for(var spot=0; spot<answers.length; spot++)
+{
+    answerButtons[spot].innerHTML = answers[spot];
+
+    answerButtons[spot].addEventListener('click', function() {
+        if(this.innerHTML == answers[correct]){
+            this.setAttribute('style','background-color:rgb(76,175,80);');
+        }else {
+            this.setAttribute('style','background-color:rgb(215,50,35);');
+        }
+    });
+}
 
 document.getElementById('question').innerHTML = question;
 
 function changeColor() {
     document.getElementById('demo2').setAttribute('style','background-color:rgb(210,50,35);')
+}
+
+function disableAll() {
+    for(var num=0; num<answerButtons.length; num++) {
+        answerButtons[num].setAttribute('disabled',true);
+    }
 }
